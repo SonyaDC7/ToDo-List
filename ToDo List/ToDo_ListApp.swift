c083127a3af0240f300e264d0cc2530ce7eaaa62
Sonya <sonyacisse7@gmail.com>
2025-08-08 12:55:25 -0400

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
